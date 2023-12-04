5ce6941e2c9f86344e6b4254a8d162611472e765
Mamun Rasid Prodhan <mrprasid47@gmail.com>
2023-12-04 19:07:02 +0530

    hello this is git

---
+++ Dicee+Challenge+-+Starting+Files/Dicee Challenge - Starting Files/index.js
@@ -0,0 +1,24 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+
+var randomDiceImage = "images/dice"+randomNumber1+".png";
+
+var image1 = document.querySelectorAll("img")[0].setAttribute("src",randomDiceImage);
+
+
+//img 2
+
+var randomNumber2 =Math.floor(Math.random()*6)+1;
+
+var randomDiceImage2 = "images/dice"+randomNumber2+".png";
+var image2 = document.querySelectorAll("img")[1].setAttribute("src",randomDiceImage2);
+
+
+//Title change 
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 wins!";
+}else if(randomNumber2>randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 wins!";
+}else{
+    document.querySelector("h1").innerHTML = "Draw!"
+}
---
+++ jquary/indx.js
@@ -0,0 +1,7 @@
+$(document).keypress(function(event){
+    $("h1").html(event.key);
+});
+
+// $("input").keypress(function(event){
+//     console.log(event.key);
+// });
